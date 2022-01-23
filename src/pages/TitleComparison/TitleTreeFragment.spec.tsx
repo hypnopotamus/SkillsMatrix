9a535459a39f11898c2970a83319a2a393be168e
cdystra@nvisia.com <cdykstra@nvisia.com>
2022-01-23 17:56:35 -0600

    back filled some more tests

--- src/pages/TitleComparison/TitleTreeFragment.spec.tsx
+++ src/pages/TitleComparison/TitleTreeFragment.spec.tsx
@@ -1,53 +1,160 @@
-import TitleTreeFragment from "./TitleTreeFragment";
-import createTheme, { Theme } from "@mui/material/styles/createTheme";
-import { useTheme } from "@mui/material/styles";
-import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
-import { titleFactory } from "./titleTree/titleFactory";
-import { addTitleEquivalents } from "./titleTree/addTitleEquivalents";
+import { Palette, PaletteColor, Theme, useTheme } from "@mui/material/styles";
+import { shallow } from "enzyme";
+import { FabricJSCanvas, FabricJSEditor, useFabricJSEditor } from "fabricjs-react";
+import { mocked } from "jest-mock";
+import { v4 as randomString } from "uuid";
+import { Title } from "../../core/titles/Title";
 import { addPromotions } from "./titleTree/addPromotions";
+import { addTitleEquivalents } from "./titleTree/addTitleEquivalents";
+import { Color } from "./titleTree/Color";
+import { titleFactory } from "./titleTree/titleFactory";
+import TitleTreeFragment from "./TitleTreeFragment";
 
-jest.mock("@mui/material/styles");
+jest.mock("@mui/material/styles", () => {
+    const realStyles = jest.requireActual("@mui/material/styles");
+
+    return {
+        ...realStyles,
+        useTheme: jest.fn()
+    }
+});
 jest.mock("fabricjs-react");
 jest.mock("./titleTree/titleFactory");
 jest.mock("./titleTree/addTitleEquivalents");
 jest.mock("./titleTree/addPromotions");
 
-const fakeUseTheme = useTheme as jest.Mock<typeof useTheme>;
-const fakeUseFabricJSEditor = useFabricJSEditor as unknown as jest.Mock<typeof useFabricJSEditor>;
-const fakeTitleFactory = titleFactory as unknown as jest.Mock<typeof titleFactory>;
-const fakeAddTitleEquivalents = addTitleEquivalents as jest.Mock<typeof addTitleEquivalents>;
-const fakeAddPromotions = addPromotions as jest.Mock<typeof addPromotions>;
+const randomColor = (): PaletteColor => {
+    return {
+        light: "",
+        main: randomString(),
+        dark: "",
+        contrastText: ""
+    };
+};
+const palette: Partial<Palette> = {
+    primary: randomColor(),
+    secondary: randomColor(),
+    grey: {
+        50: randomString(),
+        100: randomString(),
+        200: randomString(),
+        300: randomString(),
+        400: randomString(),
+        500: randomString(),
+        600: randomString(),
+        700: randomString(),
+        800: randomString(),
+        900: randomString(),
+        A100: randomString(),
+        A200: randomString(),
+        A400: randomString(),
+        A700: randomString(),
+    }
+};
+const theme: Partial<Theme> = {
+    palette: palette as Palette
+};
 
-const theme = createTheme();
-//fakeUseTheme.mockReturnValue(theme);
+const fabricOnReady = jest.fn();
+mocked(useFabricJSEditor)
+    .mockReturnValue({ selectedObjects: [], editor: undefined, onReady: fabricOnReady });
+
+const fakeTitleFactory = mocked(titleFactory);
+const fakeAddTitleEquivalents = mocked(addTitleEquivalents);
+const fakeAddPromotions = mocked(addPromotions);
 
 describe(`<TitleTreeFragment />`, () => {
     describe(`when there is no title selected`, () => {
-        it(`renders nothing`, () => {
+        const titleTreeFragment = shallow(<TitleTreeFragment selectComparison={() => { }} />);
 
+        it(`renders nothing`, () => {
+            expect(titleTreeFragment.isEmptyRender()).toBe(true);
         });
     });
 
     describe(`when there is a title selected`, () => {
-        it(`renders a fabric canvas`, () => {
+        const title: Title = {
+            title: "",
+            skills: undefined!,
+            nextLevels: [],
+            equivalentLevels: []
+        };
+        mocked(useTheme).mockReturnValue(theme);
+        const titleTreeFragment = shallow(<TitleTreeFragment title={title} selectComparison={() => { }} />);
 
+        it(`renders a fabric canvas`, () => {
+            const canvas = titleTreeFragment.find(FabricJSCanvas);
+
+            expect(canvas.prop("onReady")).toBe(fabricOnReady);
         });
 
         describe(`when the canvas editor is ready`, () => {
-            it(`clears the canvas`, () => {
+            const canvas: Partial<fabric.Canvas> = {
+                add: jest.fn(),
+                clear: jest.fn(),
+                setHeight: jest.fn()
+            };
+            const editor: Partial<FabricJSEditor> = {
+                canvas: canvas as fabric.Canvas
+            };
+            const selectComparison = jest.fn();
+            const selectedTitle: Title = {
+                title: "",
+                skills: undefined!,
+                nextLevels: [],
+                equivalentLevels: []
+            };
+            const titleComparison: Title = {
+                title: "",
+                skills: undefined!,
+                nextLevels: [],
+                equivalentLevels: []
+            };
+            const unselectedColor: Color = {
+                light: theme.palette!.grey[300],
+                dark: theme.palette!.grey[600]
+            };
+            const selectedTitleGroup: Partial<fabric.Group> = {};
 
+            beforeEach(() => {
+                fakeTitleFactory.mockReturnValue(selectedTitleGroup as fabric.Group);
+                mocked(useTheme).mockReturnValue(theme);
+                mocked(useFabricJSEditor)
+                    .mockReturnValueOnce({ selectedObjects: [], editor: editor as FabricJSEditor, onReady: fabricOnReady });
+
+                shallow(<TitleTreeFragment title={selectedTitle} comparison={titleComparison} selectComparison={selectComparison} />);
+            });
+
+            it(`clears the canvas`, () => {
+                expect(canvas.clear).toHaveBeenCalledTimes(1);
             });
 
             it(`adds the selected title in the primary theme color`, () => {
-
+                expect(fakeTitleFactory).toHaveBeenCalledWith(selectedTitle, theme.palette!.primary);
+                expect(canvas.add).toHaveBeenCalledWith(selectedTitleGroup);
             });
 
             it(`adds equivalent titles to the selected one`, () => {
-
+                expect(fakeAddTitleEquivalents).toHaveBeenCalledWith(
+                    canvas,
+                    selectedTitle,
+                    titleComparison,
+                    selectedTitleGroup,
+                    theme.palette!.secondary,
+                    unselectedColor,
+                    selectComparison,
+                );
             });
 
             it(`adds promotions from the selected title`, () => {
-
+                expect(fakeAddPromotions).toHaveBeenCalledWith(
+                    canvas,
+                    selectedTitle,
+                    titleComparison,
+                    theme.palette!.secondary,
+                    unselectedColor,
+                    selectComparison,
+                );
             });
         });
     });
--- src/pages/TitleComparison/titleTree/addPromotions.test.ts
+++ src/pages/TitleComparison/titleTree/addPromotions.test.ts
@@ -1,7 +1,61 @@
 import { addPromotions } from "./addPromotions";
+import { fabric } from "fabric";
+import { connect } from "./connect";
+import { mocked } from "ts-jest/utils";
+import { Title } from "../../../core/titles/Title";
+import { titleFactory } from "./titleFactory";
+
+jest.mock("./connect");
+jest.mock("./titleFactory");
+
+const fakeConnect = mocked(connect);
+const fakeTitleFactory = mocked(titleFactory);
 
 describe(`addPromotions`, () => {
-    it(`needs to be tested`, () => {
-        expect(true).toBe(false);
+    const itConnectsOnCanvas = (canvas: fabric.Canvas, from: fabric.Object, to: fabric.Object) =>
+        it(`connects from to to on the canvas`, () => {
+            expect(fakeConnect).toHaveBeenCalledWith(canvas, from, to);
+        });
+
+    const describePromotionsFrom = (title: Title) => {
+        describe(`when there is an even number`, () => {
+            it(`adds them alternating left and right above`, () => {
+
+            });
+        });
+
+        describe(`when there is an odd number`, () => {
+            it(`adds the first one straight above then alternates left and right above`, () => {
+
+            });
+        });
+
+        describe(`when a promotion is the selected comparison`, () => {
+            it(`is added in the comparison color`, () => {
+
+            });
+
+            it(`is unselectable`, () => {
+
+            });
+        });
+
+        describe(`promotions that are not the selected comparison`, () => {
+            it(`is added in the unselected color`, () => {
+
+            });
+
+            it(`is selectable`, () => {
+
+            });
+        });
+    }
+
+    describe(`promotions from the selected title`, () => {
+        
+    });
+
+    describe(`promotions from an equivalent title`, () => {
+        
     });
 });
