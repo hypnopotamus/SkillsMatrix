941e691dd3398c79b233665ac25a977c39ef778e
christopher dykstra <chrdyks@gmail.com>
2022-11-19 15:57:03 -0600

    get verdaccio to listen on all hosts, hopefully fix the "host.docker.internal" issues in npm install on both windows and mac

--- buildScripts/src/buildContainer.ts
+++ buildScripts/src/buildContainer.ts
@@ -10,10 +10,12 @@ interface TagList {
 const containerName = process.env.npm_package_config_container_name;
 const registry = process.env.npm_package_config_container_registry;
 const version = process.env.npm_package_version;
+const packageRegistry = process.env.npm_package_config_container_npm_registry;
 
 if (!containerName) throw new Error("add config.container.name in package.json");
 if (!registry) throw new Error("add config.container.registry in package.json");
 if (!version) throw new Error("add version in package.json");
+if (!packageRegistry) throw new Error("add config.container.npm.registry in package.json");
 
 const tag = `${containerName}:${version}`;
 const registryTag = `${registry}/${tag}`;
@@ -31,7 +33,7 @@ const buildImage = async (): Promise<void> => {
             "build",
             "--tag", `${containerName}:latest`,
             "--tag", tag,
-            "--network", "host",
+            "--build-arg", `REGISTRY=${packageRegistry}`,
             "."
         ],
         {}
